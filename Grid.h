d8ba5f2822755c15ad9665b1a75298ed4cd466bc
Hugo <huhernandez@csumb.edu>
2016-12-03 14:17:01 -0800

    grid update

--- Grid.h
+++ Grid.h
@@ -5,9 +5,34 @@
 //  Created by Nestor Zepeda on 12/3/16.
 //  Copyright © 2016 csumb-lhd. All rights reserved.
 //
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
 
 #ifndef Grid_h
 #define Grid_h
 
+#define ROW 10  //defining number of rows
+#define COL 10  //defining number of columns
+
+
+
+class Grid
+{
+private:
+    Grid();     //constructor
+
+    char board[ROW][COL];
+    char letters[8];
+    char abc[10];
+    char numberside[10];
+    
+public:
+    void update(int x, int y, int z);
+    void display();
+    bool checkmove(int x,int y);
+    
+};
 
 #endif /* Grid_h */
