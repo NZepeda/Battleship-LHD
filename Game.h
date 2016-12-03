52550235daff030ed6933c7a9722d6f103ee26b7
Will Czubiak <Will Czubiak>
2016-12-03 10:29:22 -0800

    Created Game class and called it from main

---
+++ Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Battleship-LHD
+//
+
+//  Copyright © 2016 csumb-lhd. All rights reserved.
+//
+
+#ifndef Game_h
+#define Game_h
+
+
+#endif /* Game_h */
--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 /*
- *
- *
- *
+ * Authors:
+ * Info: Text based Battleship game.
+ * Date: 12/3/16
  */
 
-#include <iostream>
+#include "Game.h"
 
 using namespace std;
 
 int main(){
 	
-	
+	Game game();
+	game.start();
 	
 	return 0;
 }
