55d37baf71553990a822cd1917bdea591c72cec2
nzepeda <NestorDZepeda@gmail.com>
2016-12-03 11:28:22 -0800

    Added skeleton code for the ship class

--- Ship.cpp
+++ Ship.cpp
@@ -1,3 +1,22 @@
 
-
 #include "Ship.h"
+
+// Constructor
+Ship::Ship(int s, string n){
+    name = n;
+    size = s;
+}
+
+
+int Ship::getSize(){
+    return size;
+    
+}
+
+
+bool Ship::check(int xCoor, int yCoor){
+    bool hit = false;
+    
+    
+    return hit;
+}
